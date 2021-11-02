3358c09b52dfa607a0556367c428094e06b71751
erik savage <erik.k.savage@gmail.com>
2021-11-02 15:10:28 -0700

    added function genTable

--- js/app.js
+++ js/app.js
@@ -54,6 +54,60 @@ Store.prototype.renderLedger = function (){
         ulEl.appendChild(liEl);
         liEl.textContent = `Total: ${this.totalSales}`;
 };
+// render to table
+function genTable(){
+    const tableHeader = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', 'Daily Location Totals'];
+    
+    const mainEl = document.querySelector('main');
+    const sectionEl = document.createElement('section');
+    mainEl.appendChild(sectionEl);
+    sectionEl.setAttribute('class', 'ledgertabel');
+    const tableEl = document.createElement('table');
+    sectionEl.appendChild(tableEl);
+
+    for(let i = -1; i < tableHeader.length; i += 1){
+        if (i === -1){
+            const thEl = document.createElement('th');
+            tableEl.appendChild(thEl);
+        } else {
+            const thEl = document.createElement('th');
+            tableEl.appendChild(thEl);
+            thEl.setAttribute('scope', 'col');
+            thEl.textContent = tableHeader[i];
+        }
+    }
+}
+
+
+
+// Store.prototype.renderTableLedger = function (){
+
+//     const h2El = document.createElement('h2');
+//     sectionEl.appendChild(h2El);
+//     h2El.textContent = `${this.location}`;
+
+//     const ulEl = document.createElement('ul');
+//     sectionEl.appendChild(ulEl);
+//     for (let i = 0; i < this.estDailySales.length; i += 1){
+//         if (i < 6){
+//             const liEl = document.createElement('li');
+//             ulEl.appendChild(liEl);
+//             liEl.textContent = `${i + 6}am: ${this.estDailySales[i]} cookies`;
+//             // console.log(ulEl.textContent);
+//         } else if (i === 6){
+//             const liEl = document.createElement('li');
+//             ulEl.appendChild(liEl);
+//             liEl.textContent = `${i + 6}pm: ${this.estDailySales[i]} cookies`;
+//         } else {
+//             const liEl = document.createElement('li');
+//             ulEl.appendChild(liEl);
+//             liEl.textContent = `${i - 6}pm: ${this.estDailySales[i]} cookies`;
+//         }    
+//     }
+//     const liEl = document.createElement('li');
+//         ulEl.appendChild(liEl);
+//         liEl.textContent = `Total: ${this.totalSales}`;
+// };
 
 function Store (location, minHrlyCust, maxHrlyCust, avgCustSale, estDailySales, totalSales) {
     this.location = location,
@@ -76,5 +130,7 @@ tokyoStore.renderLedger();
 dubaiStore.renderLedger();
 parisStore.renderLedger();
 limaStore.renderLedger();
+
+genTable();
 
 
